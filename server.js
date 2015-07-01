9623e2f03d32583b7dfd087c1842752ac297eea3
ArgentumVir <argentumvir@gmail.com>
2015-07-01 15:47:13 -0700

    Testing Termina

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
 
-//serve static content for the app from the 'pages' directory in the app dir
 app.use(express.static('./apps'));
 app.listen(80);
